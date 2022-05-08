805b8669f8411cd8f188ff4885cf1253306c9e4a
Redensarten <jan.steffes@hotmail.de>
2022-05-08 16:03:53 +0200

    Add Answering to udp broadcasts

--- ScanServer_NetCore/Program.cs
+++ ScanServer_NetCore/Program.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ScanServer_NetCore
 {
     public class Program
     {
+        private const int httpPort = 6933;
+        private const int httpsPort = 44309;
+
         public static void Main(string[] args)
         {
+            CreateBroadCastRecievers();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -22,9 +32,40 @@ namespace ScanServer_NetCore
                 {
                     webBuilder
                     .UseIISIntegration()
-                    .UseUrls("https://0.0.0.0:44309", "http://0.0.0.0:6933")
+                    .UseUrls($"https://0.0.0.0:{httpsPort}", $"http://0.0.0.0:{httpPort}")
                     .UseStartup<Startup>();
                     
                 });
+
+        public static void CreateBroadCastRecievers()
+        {
+            StartReceiveThread(httpPort);
+            StartReceiveThread(httpsPort);
+        }
+
+        private static void StartReceiveThread(int port)
+        {
+            var httpsListener = new UdpClient(port);
+            var httpsEndpoint = new IPEndPoint(IPAddress.Any, port);
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Debug.WriteLine($"==> [Port {port}] Waiting to receive bytes..");
+                        var bytes = httpsListener.Receive(ref httpsEndpoint);
+                        Debug.WriteLine($"==> [Port {port}] Got {bytes.Length} bytes, answering...");
+                        _ = httpsListener.SendAsync(bytes, bytes.Length);
+                        Debug.WriteLine($"==> [Port {port}] Did send same bytes back");
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine($"==> [Port {port}] Exception while recieving/answering broadcast: {e.Message}");
+                    }
+                }
+            }).Start();
+        }
+
     }
 }
